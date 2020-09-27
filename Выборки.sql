aff7cbc438583a4280b751eecfbd6fdf3e4d215c
Danil <Baraboshkin2001@mail.ru>
2020-09-28 00:17:11 +0500

    deploy

---
+++ Выборки.sql
@@ -0,0 +1,11 @@
+select * from `резюме` where `профессия` = 1 ;
+select * from `резюме` where `профессия` = 2 ;
+SELECT * from `вакансия` where `профессия` = 1;
+SELECT * FROM `вакансия` WHERE `город`= 1;
+SELECT * FROM `организация` WHERE `город` = 1;
+select * FROM `вакансия` where `профессия` = 1 or `профессия` = 2 and `заработная плата` < 25000.00;
+select * FROM `вакансия` where `профессия` = 1 and `заработная плата` > 15000.00;
+SELECT `ID` from `Избранные вакансии` WHERE `Вакансия` = 1;
+SELECT `ID` from `Избранные резюме` WHERE `ID резюме` = 1;
+SELECT TOP 3 * FROM `Избранные вакансии`;
+SELECT TOP 3 * FROM `Избранные резюме`;
